b5eb58867a74fc7b36c30e5dac8200fc1093f64c
franklingu <franklingujunchao@gmail.com>
2014-11-02 17:43:53 +0800

    change exception code

--- asm_mips_single_exception.asm
+++ asm_mips_single_exception.asm
@@ -1,6 +1,15 @@
 # test exception
-start:	lui $t0, 0x7fff
-	ori $t0, 0xffff
+reset: j start
+exc_handler: lui $t0, 0x1002
+	ori $t0, 0x0000
+	lui $t3, 0x0000
+	ori $t3, 0xaaaa
+	sw  $t3, 0($t0)
+die:	j die
+start:	lui $t2, 0x7fff
+	ori $t2, 0xffff
 	lui $t1, 0x7fff
 	ori $t1, 0xffff
-	add $t0, $t1, $t0
+	add $t2, $t1, $t2
+	ori $t0, 0xffff
+	j reset
